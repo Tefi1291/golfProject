4bfb0c033d88d8ccda6046d84095cc8dd8ceb608
estefania <tefi.rollino@gmail.com>
2019-08-13 16:02:34 +1200

    Api Contracts + Managers

---
+++ Core/Managers/OrderManager.cs
@@ -0,0 +1,12 @@
+﻿using Core.Contracts.Managers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Managers
+{
+    public class OrderManager: IOrderManager
+    {
+
+    }
+}
